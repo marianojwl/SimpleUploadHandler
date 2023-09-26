91bdd3bb042f65126633a37134ec8df1b16e248d
Mariano Soto <marianojwl@gmail.com>
2023-09-26 15:10:47 -0300

    bug fixes

--- SimpleUploadHandler.php
+++ SimpleUploadHandler.php
@@ -16,7 +16,7 @@ namespace marianojwl\SimpleUploadHandler {
             return $html;
           }
           
-          public function handleUpload(string $inputFileName, string $target_dir = "./") {
+          public function handleUpload(string $inputFileName, array $permited = [], string $target_dir = "./") {
             $this->abortIfFileToBig();
             // Check if image file is a actual image or fake image
             $fileNamesResult = [];
@@ -29,7 +29,6 @@ namespace marianojwl\SimpleUploadHandler {
                 $countfiles = count($_FILES[$inputFileName]['name']);
                 for($i=0;$i<$countfiles;$i++)
                 {
-                  $fileNamesResult[] = $_FILES[$inputFileName]['name'][$i];
                   $uploadOk = 1;
                   $target_file = $target_dir . basename($_FILES[$inputFileName]["name"][$i]);
                   $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -52,19 +51,22 @@ namespace marianojwl\SimpleUploadHandler {
                     $uploadOk = 0;
                   }
                   // Allow certain file formats
-                  if($imageFileType != "png") {
-                    echo "Sorry, only PNG files are allowed.";
+                  if(!in_array($imageFileType, $permited, true)) {
+                    echo "Sorry, only ".implode(", ",$permited)." files are allowed.";
                     $uploadOk = 0;
                   }
                   // Check if $uploadOk is set to 0 by an error
                   if ($uploadOk == 0) {
                     echo "Sorry, file ".$_FILES[$inputFileName]["name"][$i]." was not uploaded.";
-                  // if everything is ok, try to upload file
+                    $fileNamesResult[] = null;
+                    // if everything is ok, try to upload file
                   } else {
                     if (move_uploaded_file($_FILES[$inputFileName]["tmp_name"][$i], $target_file)) {
                     echo "The file ". htmlspecialchars( basename( $_FILES[$inputFileName]["name"][$i])). " has been uploaded.";
+                    $fileNamesResult[] = $target_file;
                     } else {
                     echo "Sorry, there was an error uploading ".$_FILES[$inputFileName]["name"][$i]." file.";
+                    $fileNamesResult[] = null;
                     }
                   }
           
